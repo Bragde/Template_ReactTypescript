3f06c5305d62711293b9ca837f19d625f1fa024a
Daniel Lundholm <Daniel7805@hotmail.com>
2020-11-13 08:44:43 +0100

    Utveckling react table

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { Column } from "react-table";
+import Table from "./Table";
+import {PubOverview} from "../types/types"
 
-export const Home = () => (
+export const Home = () => {
+    const columns: Column<PubOverview>[] = React.useMemo(() => [
+        {
+            Header: "Namn",
+            accessor: "name"
+        },
+        {
+            Header: "Folk",
+            accessor: "people"
+        },
+        {
+            Header: "Atmosfär",
+            accessor: "enviroment"
+        },
+        {
+            Header: "Mat",
+            accessor: "food"
+        },
+        {
+            Header: "Öl",
+            accessor: "beer"
+        }
+    ],[])
 
-
-    
-    <h1>HOME COMPONENT</h1>
-)
+    return <Table columns={columns} />
+}
---
+++ src/types/types.tsx
@@ -0,0 +1,7 @@
+export type PubOverview = {
+    name: string,
+    pepole: number,
+    enviroment: number,
+    food: number,
+    beer: number
+}
